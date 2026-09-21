d1b88671770c5c160295524e5e86b6631b70e6b6
Adel Amr <adel3amr@gmail.com>
2026-09-21 14:37:24 +0200

    Add log loss to PD validation diagnostics

--- src/validation.py
+++ src/validation.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.metrics import roc_auc_score, roc_curve, brier_score_loss
+from sklearn.metrics import roc_auc_score, roc_curve, brier_score_loss, log_loss
 
 
 def gini(y_true, y_prob):
@@ -42,6 +42,7 @@ def validation_summary(y_true, y_prob):
         "Gini": gini(y_true, y_prob),
         "KS": ks_statistic(y_true, y_prob),
         "Brier": brier_score_loss(y_true, y_prob),
+        "LogLoss": log_loss(y_true, y_prob, labels=[0, 1]),
         "Mean_PD": float(np.mean(y_prob)),
         "Observed_DR": float(np.mean(y_true)),
         "Calibration_in_the_large": calibration_in_the_large(y_true, y_prob),
