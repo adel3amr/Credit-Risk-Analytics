eabddc4c44db7a45d5969a488249ed0bab9e4f67
Adel Amr <adel3amr@gmail.com>
2026-09-23 15:23:35 +0200

    Benchmark LGD challengers on fixed holdout

--- notebooks/lgd_model_pipeline.py
+++ notebooks/lgd_model_pipeline.py
@@ -11,6 +11,8 @@ from lgd_model import (
     gradient_boosting_lgd_model, ridge_lgd_challenger,
     huber_gradient_boosting_lgd_challenger, random_forest_lgd_challenger,
     hist_gradient_lgd_challenger,
+    huber_gradient_boosting_lgd_challenger, random_forest_lgd_challenger,
+    hist_gradient_lgd_challenger,
     predict_lgd, lgd_validation_summary, calibration_table, save_model,
 )
 
@@ -33,6 +35,9 @@ train_idx,test_idx=train_test_split(
 models={
     "Gradient Boosting":gradient_boosting_lgd_model(),
     "Ridge":ridge_lgd_challenger(),
+    "Huber Gradient Boosting":huber_gradient_boosting_lgd_challenger(),
+    "Random Forest":random_forest_lgd_challenger(),
+    "Histogram Gradient Boosting":hist_gradient_lgd_challenger(),
     "GB Huber":huber_gradient_boosting_lgd_challenger(),
     "Random Forest":random_forest_lgd_challenger(),
     "Hist Gradient":hist_gradient_lgd_challenger(),
